111bafdcb44290d7c607a41672bc52ec0a839447
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-08-12 14:08:30 -0600

    primer commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export const MONGODB_URI = process.env['MONGODB_URI']
+
+
+export const CLOUDINARY_CLOUD_NAME = process.env['CLOUDINARY_CLOUD_NAME']
+export const CLOUDINARY_CLOUD_KEY = process.env['CLOUDINARY_CLOUD_KEY']
+export const CLOUDINARY_CLOUD_SECRET = process.env['CLOUDINARY_CLOUD_SECRET']
---
+++ utils/mongoose.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import {MONGODB_URI} from '../config.js'
+
+
+export async function connectToDB() {
+  try {
+    await mongoose.connect(MONGODB_URI)
+    console.log('Mongodb conectado')
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
