a6959c4635f185b2e486c759375a16e87bf43c5d
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-08-12 15:19:14 -0600

    cuarto commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import app from './app.js'
 import { connectToDB } from './utils/mongoose.js'
 
+
+//var express = require('express');
+//var app = express();
+
+//app.set('port', (process.env.PORT || 3000))
+
+//app.get('/', function(req, res) {
+//  var result = 'Funcionando'
+//  res.send(result);
+//}).listen(app.get('port'), function(){
+ // console.log('Corriendo en puerto', app.get('port'));
+//});
+
 async function main() {
   await connectToDB()
-  app.listen(3000)
+  app.listen(process.env.PORT || 3000)
   console.log('Funcionando en ', 3000)
 }
 
