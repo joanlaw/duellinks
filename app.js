c79ee478e274dede58321d095238b329130ba0e9
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-08-17 02:51:16 -0600

    agregando fileupload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 
 import indexRoutes from './routes/index.routes.js'
 import cartasRoutes from './routes/cards.routes.js'
+import fileUpload from 'express-fileupload'
 
 const app = express()
 //const cors = require('cors')
@@ -12,6 +13,9 @@ const app = express()
 
 app.use(cors())
 app.use(morgan('dev'))
+app.use(fileUpload({
+    tempFileDir: '/uploads'
+}))
 app.use(express.json())
 app.use(indexRoutes)
 app.use(cartasRoutes)
