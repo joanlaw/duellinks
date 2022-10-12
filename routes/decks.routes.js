653bf0d21b63dfab76a785f55e21f6ada6782d6b
Oscar Gonzalez <oscar.joanlouis@gmail.com>
2022-10-12 08:51:01 -0600

    parametros para crear decks

--- routes/decks.routes.js
+++ routes/decks.routes.js
@@ -10,7 +10,7 @@ import {
 const routerdecks = Router()
 
 //import fileUpload from 'express-fileupload'
-import { getDecks } from '../controllers/decks.controllers'
+//import { getDecks } from '../controllers/decks.controllers'
 
 routerdecks.get('/decks', getDecks)
 routerdecks.post('/decks', createDecks)
